7f3c0cedd414a4c567642791a4e142df921c9057
Sarvar Abduqodirov <abduqodirovsarvar.2002@gmail.com>
2024-03-07 12:05:48 +0500

    added some services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../header/header.component';
+import { HomeComponent } from '../home/home.component';
+import { AboutComponent } from '../about/about.component';
+import { ExperienceComponent } from '../experience/experience.component';
+import { EducationComponent } from '../education/education.component';
+import { StackComponent } from '../stack/stack.component';
+import { ProjectComponent } from '../project/project.component';
+import { ContactComponent } from '../contact/contact.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, HomeComponent, AboutComponent,
+            ExperienceComponent, EducationComponent, StackComponent, ProjectComponent,
+            ContactComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
